b6974e49c0c16d73a45e96e9fe13f093eaa29f4e
xutao <979855682@qq.com>
2017-04-01 09:41:40 +0800

    看看

---
+++ ssmgittest/src/ssmgittest/bayin/Commit.java
@@ -0,0 +1,16 @@
+package ssmgittest.bayin;
+/**
+*@author 作者：xutao
+* @date 创建时间：2017年4月1日 上午9:39:41
+* @version 1.0
+* @parameter
+* @since
+* @return
+
+*/
+public class Commit {
+	
+	public static void main(String[] args) {
+		System.out.println("what you say??");
+	}
+}
